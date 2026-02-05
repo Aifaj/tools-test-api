716a22f5aead828c0b7d034dffc13fe19124a15c
Aifaj <117112535+Aifaj@users.noreply.github.com>
2026-02-06 01:09:08 +0530

    ralway deploy

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
 app.use('/video', videoRoutes);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
